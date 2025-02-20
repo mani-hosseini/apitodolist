2981c991d7aff09e1c7848e86f1edd2437148446
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-02-20 12:57:25 +0330

    get data api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import {useState, useEffect} from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+    const [todos, setTodos] = useState([]);
 
-  return (
-    <>
+    useEffect(() => {
+        axios
+            .get("https://dummyjson.com/todos")
+            .then((response) => {
+                setTodos(response.data.todos);
+            })
+            .catch((error) => {
+                console.error("Error fetching todos:", error);
+            });
+    }, []);
 
-    </>
-  )
-}
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-4xl font-bold">
+                Todo <span className="text-teal-400">List</span>
+            </h1>
+            <section className="mt-7 flex flex-col items-center gap-3">
+                {todos.map((todo) => (
+                    <div key={todo.id}
+                         className="flex items-center justify-between w-80 h-16 px-4 border border-black rounded-lg bg-[#F5D1D4]">
+                        <p className="text-sm">{todo.todo}</p>
+                    </div>
+                ))}
+            </section>
+        </div>
+    );
+};
 
-export default App
+export default App;
